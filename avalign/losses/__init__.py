56bb8390e233f2117f4ead5e9922b18fdc58255c
Jenna Wang <291128221+stella-sage553@users.noreply.github.com>
2024-05-14 22:01:09 +0800

    feat(losses): export public loss API

--- avalign/losses/__init__.py
+++ avalign/losses/__init__.py
@@ -1,1 +1,18 @@
 """Contrastive loss functions (NumPy reference implementations)."""
+
+from avalign.losses.functional import (
+    cosine_similarity_matrix,
+    l2_normalize,
+    log_softmax,
+    softmax,
+)
+from avalign.losses.info_nce import info_nce, symmetric_info_nce
+
+__all__ = [
+    "softmax",
+    "log_softmax",
+    "l2_normalize",
+    "cosine_similarity_matrix",
+    "info_nce",
+    "symmetric_info_nce",
+]
